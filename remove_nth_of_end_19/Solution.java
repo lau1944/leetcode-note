628b9169a2eab548c7355b52a3bcee0ac97b3728
lau1944 <a954723219@gmail.com>
2022-08-17 12:07:39 +0800

    feat: add rm nth node

--- remove_nth_of_end_19/Solution.java
+++ remove_nth_of_end_19/Solution.java
@@ -12,32 +12,26 @@ public class Solution {
             return null;
         }
 
-        ListNode[] target = findLast(head, n);
-
-        if (target[0].next == head) {
-            return head.next;
-        }
-
-        target[0].next = target[1].next;
-        return head;
+        ListNode dump = new ListNode(-1);
+        dump.next = head;
+        ListNode target = findLast(dump, n + 1);
+        target.next = target.next.next;
+        return dump.next;
     }
 
-    ListNode[] findLast(ListNode head, int n) {
+    ListNode findLast(ListNode head, int n) {
         ListNode p = head;
         for (int i = 0; i < n; ++i) {
             p = p.next;
         }
-        ListNode prev = new ListNode(-1);
+
         ListNode target = head;
-        prev.next = target;
         while (p != null) {
-            prev.next = target;
-            prev = prev.next;
             target = target.next;
             p = p.next;
         }
 
-        return new ListNode[]{prev, target};
+        return target;
     }
 }
 
